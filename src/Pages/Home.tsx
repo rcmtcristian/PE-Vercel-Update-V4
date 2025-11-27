01015e08a6f8f636dfe56bf82ddf3af738cc2619
rcmtcristian <rcmtcristian@gmail.com>
2025-11-26 20:34:46 -0500

    ✨ Feat: added lenis and framer motion libraries
    added smooth scrolling, and updated how the sticky scroll was inplemented

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,11 +3,12 @@ import ResponsiveHeroSection from "../Home/HomeHero/ResponsiveHeroSection";
 import HomeInfoSection from "../Home/Info/HomeInfoSection";
 import HomeLogosSection from "../Home/Logos/HomeLogosSection";
 import Stats from "../Home/HomeStats";
-import HomeCtaSection from "../Home/HomeCta/HomeCtaSection";
-import HomeImpactSectionSticky from "@/Home/Impact/HomeImpactSectionSticky";
+import HomeImpactSectionStickylenis from "@/Home/Impact/HomeImpactSectionStickyMotion";
 import EnvironmentalToolsSection from "@/Home/EnvironmentalToolsSection";
 import NewsletterSection from "@/Home/HomeCta/NewsletterSection";
-import HomeImpactSectionStickyWhole from "@/Home/Impact/HomeImpactSectionStickyWhole";
+import SmoothScrolling from "@/SmoothScrolling";
+import HomeImpactSectionSticky from "@/Home/Impact/HomeImpactSectionSticky";
+// import HomeImpactSectionStickyWhole from "@/Home/Impact/HomeImpactSectionStickyWhole";
 
 export interface HomeProps {
   className?: string;
@@ -15,27 +16,31 @@ export interface HomeProps {
 
 export const Home = ({ className, ...props }: HomeProps): JSX.Element => {
   return (
-    <div
-      className={
-        "bg-color-palette-primary-white flex flex-col gap-0 items-center justify-start relative overflow-hidden " +
-        className
-      }
-    >
-      <ResponsiveHeroSection></ResponsiveHeroSection>
-      <HomeInfoSection></HomeInfoSection>
+    <SmoothScrolling>
+      <div
+        className={
+          "bg-color-palette-primary-white flex flex-col gap-0 items-center justify-start relative overflow-hidden " +
+          className
+        }
+      >
+        <ResponsiveHeroSection></ResponsiveHeroSection>
+        <HomeInfoSection></HomeInfoSection>
 
-      <HomeLogosSection></HomeLogosSection>
-      <Stats></Stats>
+        <HomeLogosSection></HomeLogosSection>
+        <Stats></Stats>
 
-      {/* <HomeImpactSection></HomeImpactSection> */}
-      <HomeImpactSectionSticky></HomeImpactSectionSticky>
-      {/* <HomeImpactSectionStickyWhole></HomeImpactSectionStickyWhole> */}
-      <EnvironmentalToolsSection></EnvironmentalToolsSection>
+        {/* <HomeImpactSection></HomeImpactSection> */}
+        {/* <HomeImpactSectionSticky></HomeImpactSectionSticky> */}
+        <HomeImpactSectionStickylenis></HomeImpactSectionStickylenis>
 
-      {/* <HomeCtaSection></HomeCtaSection> */}
-      <NewsletterSection></NewsletterSection>
+        {/* <HomeImpactSectionStickyWhole></HomeImpactSectionStickyWhole> */}
+        <EnvironmentalToolsSection></EnvironmentalToolsSection>
 
-      <HomeFooter />
-    </div>
+        {/* <HomeCtaSection></HomeCtaSection> */}
+        <NewsletterSection></NewsletterSection>
+
+        <HomeFooter />
+      </div>
+    </SmoothScrolling>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path"
 import { defineConfig } from 'vite'
+// Type resolution for @vitejs/plugin-react can fail under some TS moduleResolution settings;
+// suppress the TS error here or update tsconfig.json to use "moduleResolution": "node16" | "nodenext".
+ // @ts-ignore: module resolution mismatch for @vitejs/plugin-react
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
